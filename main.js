f4b636435d095602a33deabd712816401b915271
vik-kalsi <107356027+vik-kalsi@users.noreply.github.com>
2023-10-28 00:19:57 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,23 @@
+$("#inputTask").keydown(function(){
+    if ($("#inputTask").val().length >= 2) {
+            $("#addTaskBtn").removeClass("hidden");
+        } else {
+            $("#addTaskBtn").addClass("hidden");
+        }
+});
+
+
+//Add Button get clicked which starts the function
+$("#addTaskBtn").click(function(){
+    //hides the Add Task button
+    $("#addTaskBtn").addClass("hidden");
+
+    //take the inputted value and append it
+    $("ul").append("<li> <input type=\"radio\" class=\"md:scale-[2] md:mr-3\"> <label>" + $("#inputTask").val() + "</label> <button type=\"button\" class=\"bg-[#982222] rounded-2xl px-2 text-white md:p-2 hover:bg-red-950\"> Delete </button> </li>");
+
+    //clears the inputted value
+    $("#inputTask").val("");
+
+});
+
+
